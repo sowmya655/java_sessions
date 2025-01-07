a6c0a91457814339c2404ceee69a554361518172
sowmya655 <sowmyapapasani9390@gmail.com>
2025-01-07 20:47:53 +0530

    Printing Of numbers

---
+++ Day5_Loop.java
@@ -0,0 +1,32 @@
+public class Day5_Loop {
+   public static void main(String[] args) {
+       System.out.println("Hello");
+       System.out.println("Hello");
+       System.out.println("Hello");
+       System.out.println("Hello");
+       System.out.println("Hello");
+       System.out.println("Hello");
+       System.out.println("Hello");
+       System.out.println("Hello");
+       //count = 0
+       //count = 1
+       //4
+       //count = 2
+       //count = 3
+       //count = 4
+
+       //Loop
+       //1.initialization
+       //condition
+       //incrementation/decrement
+
+       for(int count = 0; count <= 4;count++){
+        System.out.println(count);
+
+       }
+       for(int i = 20; i<=35; i++){
+        System.out.println(i);
+       }
+       
+   } 
+}
